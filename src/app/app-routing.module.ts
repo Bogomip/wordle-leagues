6ce24d92a0567c6ce0759481f86d90aff3968f99
Bogomip <alex.bunting@gmail.com>
2022-01-29 22:11:04 +0300

    design90pc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './leagues/create/create.component';
+import { LeaguesComponent } from './leagues/leagues.component';
+import { AddResultsComponent } from './user/add-results/add-results.component';
+import { LoginComponent } from './utilities/login/login.component';
+import { MenuComponent } from './utilities/menu/menu.component';
+import { NotfoundComponent } from './utilities/notfound/notfound.component';
+import { RulesComponent } from './utilities/rules/rules.component';
+import { SignupComponent } from './utilities/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: MenuComponent, outlet: 'left' },
+    { path: '', component: RulesComponent },
+    { path: 'rules', component: RulesComponent },
+    { path: 'signup', component: SignupComponent, outlet: 'left' },
+    { path: 'login', component: LoginComponent, outlet: 'left' },
+    { path: 'create', component: CreateComponent, outlet: 'left' },
+    { path: 'submit', component: AddResultsComponent, outlet: 'left' },
+    { path: 'leagues', component: LeaguesComponent},
+    { path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './utilities/menu/menu.component';
+import { LeaguesComponent } from './leagues/leagues.component';
+import { SignupComponent } from './utilities/signup/signup.component';
+import { LoginComponent } from './utilities/login/login.component';
+import { RulesComponent } from './utilities/rules/rules.component';
+import { CreateComponent } from './leagues/create/create.component';
+import { NotfoundComponent } from './utilities/notfound/notfound.component';
+import { AddResultsComponent } from './user/add-results/add-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LeaguesComponent,
+    SignupComponent,
+    LoginComponent,
+    RulesComponent,
+    CreateComponent,
+    NotfoundComponent,
+    AddResultsComponent
   ],
   imports: [
     BrowserModule,
