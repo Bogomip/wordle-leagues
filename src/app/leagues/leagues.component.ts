1ce5e2fd0cf5e2f322fc62dd89f4437b8b02d22a
Bogomip <alex.bunting@gmail.com>
2022-01-31 22:39:41 +0300

    leagues dynamic!

--- src/app/leagues/leagues.component.ts
+++ src/app/leagues/leagues.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { League, LeagueMember, LeagueService } from '../services/league.service';
 
 @Component({
   selector: 'app-leagues',
@@ -7,9 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeaguesComponent implements OnInit {
 
-  constructor() { }
+    // leagues array....
+    leagues: League[];
 
-  ngOnInit(): void {
-  }
+    // how much each position is worth, starting from 1st. No fail points are available.
+    scoreArray: number[] = [2,3,4,3,2,1];
 
+    constructor(private leagueService: LeagueService) { }
+
+    ngOnInit(): void {
+        this.leagues = this.leagueService.getLeaguesData();
+        // sort each league by the total points...
+        for(let i = 0; i < this.leagues.length; i++) {
+            this.leagues[i].members = [...this.sortLeagueArray(this.leagues[i].members)];
+        }
+    }
+
+    /**
+     * Sorts the league members into descending points order...
+     * @param leagues
+     * @returns
+     */
+    sortLeagueArray(leagues: LeagueMember[]): LeagueMember[] {
+        const sortedLeagues = leagues.sort((a: LeagueMember, b: LeagueMember) => this.calculateMemberPoints(b) - this.calculateMemberPoints(a));
+        return [...sortedLeagues];
+    }
+
+    /**
+     *
+     * @param member Calculates the total points of a member
+     * @returns
+     */
+    calculateMemberPoints(member: LeagueMember): number {
+        let totalScore = 0;
+        for(const [score, value] of Object.entries(member.score)) {
+            if(score !== 'fail') {
+                totalScore += (this.scoreArray[+score-1] * value);
+            }
+        }
+        return totalScore;
+    }
+
+    toggleNotifications(): void {
+
+    }
+
+    leaveLeague(leagueId: string): void {
+        // if an admin leaves what happens?
+    }
+
+    addLeaguecodeToClipboard(leagueId: string): void {
+
+    }
+
+    restartLeague(leagueId: string): void {
+
+    }
+
+    deleteLeague(leagueId: string): void {
+
+    }
+
+    removeUserFromLeague(leagueId: string, userIdToDelete: string): void {
+
+    }
 }
