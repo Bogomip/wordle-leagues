22bfdde1ff6e53a8a2ac769209fdd0d29cb27d2f
Alex Bunting <alex.bunting@gmail.com>
2024-03-10 08:35:58 +0300

    upd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3001/'
-  // apiUrl: 'http://109.74.204.100:3001/'
-//   apiUrl: 'https://wordleleague.herokuapp.com/'
-//   apiUrl: 'http://89.211.109.144:3001/'
 };
 
 /*
